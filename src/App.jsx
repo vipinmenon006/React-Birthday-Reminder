955c9222403acd868ac1c89f46e6fea30d4e335d
vipinmenon006 <Vipinmenon16@gmail.com>
2022-04-19 21:48:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+	// create a state variable here
+    // const [currentIndex, setCurrentIndex] = useState(0);
+    const [people, setPeople] = useState(data)
+	// this should clear all records
+	function clearAllRecords(e) {
+        setPeople([])
+    }
+	return (
+		<main>
+			<section className="container">
+				{/* change this to state variable data */}
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>Clear All</button>
+			</section>
+		</main>
+	)
+}
+
+export default App
